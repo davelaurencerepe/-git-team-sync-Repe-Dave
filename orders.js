0f8cf4f6bde5662cfd111e83ad39bee28a26436a
LAB11_USER ON WS30 <L11X09W30@LabHCI.net>
2026-09-18 09:20:07 +0800

    Add VIP bonus points for orders over $100 - Repe-Dave

--- orders.js
+++ orders.js
@@ -16,7 +16,10 @@ function cancelOrder(order) {
 }
 
 function calculateLoyaltyPoints(order) {
-  return Math.floor(order.total / 10);
+  let multiplier = 1;
+  if (order.total > 100) {
+    multiplier = 1.5;
+  }
+  return Math.floor((order.total / 10) * multiplier);
 }
-
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
